e240dca1d7070ebdc4b3b691a77662fe69acf04e
Adreena Howard <adreena.howard@yahoo.com>
2018-12-31 13:10:09 -0600

    added new changes

--- keys.js
+++ keys.js
@@ -1,13 +1,7 @@
-console.log('this is loaded');
+console.log("Keys loaded");
 
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
 
-exports.omdb = {
-    id: process.env.OMDB_ID,
-  };
-  exports.bit = {
-    id: process.env.BANDSINTOWN_ID,
-  };
--- liri.js
+++ liri.js
@@ -1,65 +1,145 @@
 
 require("dotenv").config();
 
-var keys = require("./keys.js");
-var fs= require("fs");
-var request = require("request");
-var Spotify = require ("node-spotify-api");
+var Spotify = require("node-spotify-api");
+var keys = require("./keys");
+var axios = require("axios");
+var moment = require("moment");
+var fs = require("fs");
 var spotify = new Spotify(keys.spotify);
-var movieName = process.argv[3];
-var liriReturn = process.argv[2];
-
-
-//switch for different commands
-switch (liriReturn) {
-    case "spotify-this-song":
-    spotifyThisSong();
-    break;
-
-    case "movie-this":
-    movieThis();
-    break;
-
-    case "do-what-it-says":
-    doWhatItSays();
-    break;
-
-//helpful instrustions for new users on command line 
-default: console.log("\n" + "type any command after 'node liri.js': " + "\n" +
-"spotify-this-song 'any song title' " + "\n" +
-"movie-this 'any movie title' " + "\n" +
-"do-what-it-says " + "\n" +
-"Use quotes for multiword titles!");
-
+var getArtistNames = function (artist) {
+    return artist.name;
 };
-function spotifyThisSong(songName) {
-    var songName = process.argv[3];
-    if(!songName) {
-        songName = "I Want it That Way";
-    };
-    songRequest = songName;
-    spotify.search({
-        type: "track",
-        query: songRequest
-
-    },
-
-    function (err, data) {
-        if (!err) {
-            var trackInfo = data.tracks.items;
-            for(var i = 0; i < 5; i++){
-                if (trackInfo[i] != undefined) {
-                    var spotifyResults =
-                    "Artist: " + trackInfo[i].artist[0].name + "\n" +
-                    "song: " + trackInfo[i].name + "\n" +
-                    "preview URL: " + trackInfo[i].preview_url + "\n" +
-                    "Album: " + trackInfo[i].album.name + "\n"
-
-                    console.log(spotifyResults);
-                    console.log('  ');                 }
-            };
-        };
 
 
+var getMeSpotify = function (songName) {
+    if (songName === undefined) {
+        songName = "What's my age again";
+    }
+
+    spotify.search(
+        {
+            type: "track",
+            query: songName
+        },
+        function (err, data) {
+            if (err) {
+                console.log("Error occurred: " + err);
+                return;
+            }   
+
+            var songs = data.tracks.items;
+
+            for (var i = 0; i < songs.length; i++) {
+                console.log(i);
+                console.log("artist(s): " + songs[i].artists.map(getArtistNames));
+                console.log("song name: " + songs[i].name);
+                console.log("preview song: " + songs[i].preview_url);
+                console.log("album: " + songs[i].album.name);
+                console.log("-----------------------------------");
+            }
+        }
+    );
+};
+
+var getMyBands = function (artist) {
+    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+    axios.get(queryURL).then(
+        function (response) {
+            var jsonData = response.data;
+
+            if (!jsonData.length) {
+                console.log("No results found for " + artist);
+                return;
+            }
+
+            console.log("Upcoming concerts for " + artist + ":");
+
+            for (var i = 0; i < jsonData.length; i++) {
+                var show = jsonData[i];
+
+                
+                console.log(
+                    show.venue.city +
+                    "," +
+                    (show.venue.region || show.venue.country) +
+                    " at " +
+                    show.venue.name +
+                    " " +
+                    moment(show.datetime).format("MM/DD/YYYY")
+                );
+            }
+        }
+    );
+};
+
+// Function for running a Movie Search
+var getMeMovie = function (movieName) {
+    if (movieName === undefined) {
+        movieName = "Mr Nobody";
+    }
+
+    var urlHit =
+        "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=full&tomatoes=true&apikey=trilogy";
+
+    axios.get(urlHit).then(
+        function (response) {
+            var jsonData = response.data;
+
+            console.log("Title: " + jsonData.Title);
+            console.log("Year: " + jsonData.Year);
+            console.log("Rated: " + jsonData.Rated);
+            console.log("IMDB Rating: " + jsonData.imdbRating);
+            console.log("Country: " + jsonData.Country);
+            console.log("Language: " + jsonData.Language);
+            console.log("Plot: " + jsonData.Plot);
+            console.log("Actors: " + jsonData.Actors);
+            console.log("Rotten Tomatoes Rating: " + jsonData.Ratings[1].Value);
+        }
+    );
+};
+
+// Function for running a command based on text file
+var doWhatItSays = function () {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        console.log(data);
+
+        var dataArr = data.split(",");
+
+        if (dataArr.length === 2) {
+            pick(dataArr[0], dataArr[1]);
+        } else if (dataArr.length === 1) {
+            pick(dataArr[0]);
+        }
     });
 };
+
+// Function for determining which command is executed
+var pick = function (caseData, functionData) {
+    switch (caseData) {
+        case "concert-this":
+            getMyBands(functionData);
+            break;
+        case "spotify-this-song":
+            getMeSpotify(functionData);
+            break;
+        case "movie-this":
+            getMeMovie(functionData);
+            break;
+        case "do-what-it-says":
+            doWhatItSays();
+            break;
+        default:
+            console.log("LIRI doesn't know that");
+    }
+};
+
+// Function which takes in command line arguments and executes correct function accordingly
+var runThis = function (argOne, argTwo) {
+    pick(argOne, argTwo);
+};
+
+// MAIN PROCESS
+// =====================================
+runThis(process.argv[2], process.argv.slice(3).join(" "));
